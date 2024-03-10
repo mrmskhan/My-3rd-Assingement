20f3113c4e78ba8a7b939ac8bb9cb47aa596be0a
M. SALEEM KHAN <160552274+mrmskhan@users.noreply.github.com>
2024-03-11 04:47:55 +0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,206 @@
+console.log("OPERATOR IN TYPESCRIPT");
+console.log("Addition Operators");
+// FRUITS
+var apple = 12;
+var mango = 24;
+var fruits = apple + mango;
+console.log("Total Number Of Fruits", fruits);
+// VEGITABLES
+var tomato = 18;
+var potato = 12;
+var Vegitables = tomato + potato;
+console.log("Total Number Of Vegitables", Vegitables);
+// VEHICLES
+var cars = 15;
+var trucks = 4;
+var bikes = 28;
+var vehicles = cars + trucks + bikes;
+console.log("Total Number Vehicles", vehicles);
+// STARIONARY
+var pencils = 50;
+var erasor = 50;
+var pens = 50;
+var SCALES = 50;
+var stationary = pencils + erasor + pens + SCALES;
+console.log("Total Number of Stationary", stationary);
+// ELECTRICAL ITEMS
+var fridge = 1;
+var led = 1;
+var washingMachine = 1;
+var iron = 1;
+var microWave = 1;
+var laptop = 1;
+var pump = 1;
+var fans = 4;
+var lights = 18;
+var electricalItems = fridge + led + washingMachine + iron + microWave + laptop + pump + fans + lights;
+console.log("Total Numbers Of Electrical Items", electricalItems);
+console.log("Multiplication Operators");
+// FRUITS
+var applePrice = 10;
+var appleQuantity = 12;
+var mangoPrice = 10;
+var mangoQuantity = 24;
+var totalCost = (applePrice * appleQuantity) + (mangoPrice * mangoQuantity);
+console.log("Total cost of fruits Rs." + totalCost);
+// VEGITABLES
+var tomatoPrice = 3;
+var tomatoQuantity = 18;
+var potatoPrice = 2;
+var potatoQuantity = 12;
+var vegitableCost = (tomatoPrice * tomatoQuantity) + (potatoPrice * potatoQuantity);
+console.log("Total Cost of Vegitable Rs.", vegitableCost);
+// VEHICLES
+var carsPrice = 20000;
+var carsQuantity = 15;
+var trucksPrice = 40000;
+var trucksQuantity = 4;
+var bikesPrice = 15600;
+var bikesQuantity = 28;
+var vehicleCost = (carsPrice * carsQuantity) + (trucksPrice * trucksQuantity) + (bikesPrice * bikesQuantity);
+console.log("Total Cost of Vehicles Rs.", vehicleCost);
+// STATIONARY
+var pencilsPrice = 15;
+var pencilsQuantity = 50;
+var erasorPrice = 10;
+var erasorQuantity = 50;
+var pensPrice = 30;
+var pensQuantity = 50;
+var scalePrice = 20;
+var scaleQuantity = 50;
+var stationaryCost = (pencilsPrice * pencilsQuantity) + (erasorPrice * erasorQuantity) + (pensPrice * pensQuantity) + (scalePrice * scaleQuantity);
+console.log("Total Cost of Stationary", stationaryCost);
+// ELECTRICAL ITEMS
+var fridgePrice = 65000;
+var fridgeQuantity = 1;
+var ledPrice = 42000;
+var ledQuantity = 1;
+var washingMachinePrice = 25000;
+var washingMachineQuantity = 1;
+var ironPrice = 5000;
+var ironQuantity = 1;
+var microWavePrice = 12000;
+var microWaveQuantity = 1;
+var laptopPrice = 83000;
+var laptopQuantity = 1;
+var pumpPrice = 12000;
+var pumpQuantity = 1;
+var fansPrice = 7000;
+var fansQuantity = 4;
+var lightsPrice = 2000;
+var lightsQuantity = 18;
+var electricalCost = (fridgePrice * fridgeQuantity) + (ledPrice * ledQuantity) + (washingMachinePrice * washingMachineQuantity) + (ironPrice * ironQuantity) + (microWavePrice * microWaveQuantity) + (laptopPrice * laptopQuantity) + (pumpPrice * pumpQuantity) + (fansPrice * fansQuantity) + (lightsPrice * lightsQuantity);
+console.log("Total Cost of Electrical Items", electricalCost);
+console.log("Subtraction Operators");
+// FRUITS
+var fruits1 = 10;
+var fruits2 = 6;
+var fruitsSubtract = fruits1 - fruits2;
+console.log("Total Fruits after subtract", fruitsSubtract);
+// VEGITABLES
+var vegi1 = 10;
+var vegi2 = 3;
+var vegiSubtract = vegi1 - vegi2;
+console.log("Total Vegitable after subtract", vegiSubtract);
+// VEHICLES
+var vechl1 = 35;
+var vechl2 = 5;
+var vechicle_Subtract = vechl1 - vechl2;
+console.log("Total Vicheles after subtract", vechicle_Subtract);
+//STATIONARY
+var stationary_1 = 56;
+var stationary_2 = 51;
+var stationary_Subtract = stationary_1 - stationary_2;
+console.log("Total Stationary after subtract", stationary_Subtract);
+// ELECTRICAL ITEMS
+var elect_1 = 10;
+var elect_2 = 4;
+var elect_Subtract = elect_1 - elect_2;
+console.log("Total Electrical Items after subtract", elect_Subtract);
+console.log("Divition Operators");
+// FRUITS
+var apples = 30;
+var no0fPerson = 5;
+var apple1Modulest = apples / no0fPerson;
+console.log("Each person will get", apple1Modulest);
+// // VEGITABLES
+var veg1 = 10;
+var veg2 = 3;
+var vegSubtract = veg1 / veg2;
+console.log("Total Vegitable after Divition", vegSubtract);
+// VEHICLES
+var vehcl1 = 35;
+var vehcl2 = 5;
+var vechicleSubtract = vehcl1 / vehcl2;
+console.log("Total Vicheles after Divition", vechicleSubtract);
+// STATIONARY
+var stationary1 = 56;
+var stationary2 = 51;
+var stationarySubtract = stationary1 / stationary2;
+console.log("Total Stationary after Divition", stationarySubtract);
+// ELECTRICAL ITEMS
+var elect1 = 10;
+var elect2 = 4;
+var electSubtract = elect1 / elect2;
+console.log("Total Electrical Items after Divition", electSubtract);
+console.log("Modulest Operators");
+// FRUITS
+var apples1 = 21;
+var no0fPerson1 = 5;
+var appleModulest = apples1 % no0fPerson1;
+console.log("Each person will get Modulest", appleModulest);
+// // VEGITABLES
+var vegitable1 = 185;
+var vegitable2 = 12;
+var vegitableModulest = vegitable1 % vegitable2;
+console.log("Total Vegitable after Modulest", vegitableModulest);
+// VEHICLES
+var vechicle1 = 63;
+var vechicle2 = 22;
+var vechicleModulest = vechicle1 % vechicle2;
+console.log("Total Vicheles after Modulest", vechicleModulest);
+// STATIONARY
+var stationer1 = 21;
+var stationer2 = 5;
+var stationerModulest = stationer1 % stationer2;
+console.log("Total Stationary after Modulest", stationerModulest);
+// ELECTRICAL ITEMS
+var electric1 = 23;
+var electric2 = 5;
+var electricModulest = electric1 % electric2;
+console.log("Total Electrical Items after Modulest", electricModulest);
+console.log("Concatination Operators");
+// My Full Name
+var firstName = "Saleem";
+var lastName = "Khan";
+console.log(firstName + lastName);
+// My Location
+var area = "Shah Faisal Colony";
+var city = "Karachi";
+var country = "Pakistan";
+console.log(area + city + country);
+console.log("Concatination Operators With White Space");
+// My Full Name
+var first_Name = "Saleem";
+var last_Name = "Khan";
+console.log(first_Name + " " + last_Name);
+// My Location
+var area1 = "Shah Faisal Colony";
+var city1 = "Karachi";
+var country1 = "Pakistan";
+console.log(area1 + ", " + city1 + ", " + country1);
+console.log("Comperison Operators");
+console.log(15 == 15);
+console.log(15 === 15);
+console.log(10 != 10);
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 <= 10);
+console.log(2 <= 5);
+console.log(2 >= 5);
+var m = 15;
+m = m + 5;
+console.log(m);
+var n = 25;
+n += 5;
+console.log(n);
---
+++ operators.ts
@@ -0,0 +1,225 @@
+console.log("OPERATOR IN TYPESCRIPT")
+
+console.log("Addition Operators")
+// FRUITS
+const apple: number = 12
+const mango: number = 24
+const fruits:number = apple + mango
+console.log("Total Number Of Fruits",fruits) 
+// VEGITABLES
+const tomato: number = 18
+const potato: number = 12
+const Vegitables:number = tomato + potato
+console.log("Total Number Of Vegitables",Vegitables)
+// VEHICLES
+const cars: number= 15
+const trucks:number= 4
+const bikes: number= 28
+const vehicles: number=cars+trucks+bikes
+console.log("Total Number Vehicles", vehicles)
+// STARIONARY
+const pencils: number= 50
+const erasor: number= 50
+const pens: number = 50
+const SCALES: number=50
+const stationary:number=pencils + erasor + pens + SCALES
+console.log("Total Number of Stationary",stationary)
+// ELECTRICAL ITEMS
+const fridge:number=1
+const led:number=1
+const washingMachine:number=1
+const iron:number=1
+const microWave:number=1
+const laptop:number=1
+const pump:number=1
+const fans:number=4
+const lights:number=18
+const electricalItems:number=fridge+led+washingMachine+iron+microWave+laptop+pump+fans+lights
+console.log("Total Numbers Of Electrical Items",electricalItems)
+
+
+console.log("Multiplication Operators")
+// FRUITS
+let applePrice: number = 10
+const appleQuantity: number = 12
+let mangoPrice: number = 10
+const mangoQuantity: number = 24
+const totalCost: number = (applePrice * appleQuantity) + (mangoPrice * mangoQuantity)
+console.log("Total cost of fruits Rs." + totalCost)
+// VEGITABLES
+let tomatoPrice:number=3
+const tomatoQuantity:number=18
+let potatoPrice:number=2
+const potatoQuantity=12
+let vegitableCost:number=(tomatoPrice*tomatoQuantity)+(potatoPrice*potatoQuantity)
+console.log("Total Cost of Vegitable Rs.",vegitableCost)
+
+// VEHICLES
+let carsPrice:number= 20000
+const carsQuantity:number= 15
+let trucksPrice:number=40000
+const trucksQuantity:number=4
+let bikesPrice:number=15600
+const bikesQuantity:number=28
+let vehicleCost:number=(carsPrice*carsQuantity)+(trucksPrice*trucksQuantity)+(bikesPrice*bikesQuantity)
+console.log("Total Cost of Vehicles Rs.",vehicleCost) 
+
+// STATIONARY
+let pencilsPrice: number=15
+const pencilsQuantity:number=50
+let erasorPrice: number=10
+const erasorQuantity: number=50
+let pensPrice:number=30
+const pensQuantity: number=50
+let scalePrice:number=20
+const scaleQuantity:number=50
+let stationaryCost:number=(pencilsPrice*pencilsQuantity)+(erasorPrice*erasorQuantity)+(pensPrice*pensQuantity)+(scalePrice*scaleQuantity)
+console.log("Total Cost of Stationary",stationaryCost)
+
+// ELECTRICAL ITEMS
+let fridgePrice:number=65000
+const fridgeQuantity:number=1
+let ledPrice:number=42000
+const ledQuantity:number=1
+let washingMachinePrice:number=25000
+const washingMachineQuantity:number=1
+let ironPrice:number=5000
+const ironQuantity:number=1
+let microWavePrice:number=12000
+const microWaveQuantity:number=1
+let laptopPrice:number=83000
+const laptopQuantity:number=1
+let pumpPrice:number=12000
+const pumpQuantity:number=1
+let fansPrice:number=7000
+const fansQuantity:number=4
+let lightsPrice:number=2000
+const lightsQuantity:number=18
+let electricalCost:number=(fridgePrice*fridgeQuantity)+(ledPrice*ledQuantity)+(washingMachinePrice*washingMachineQuantity)+(ironPrice*ironQuantity)+(microWavePrice*microWaveQuantity)+(laptopPrice*laptopQuantity)+(pumpPrice*pumpQuantity)+(fansPrice*fansQuantity)+(lightsPrice*lightsQuantity)
+console.log("Total Cost of Electrical Items",electricalCost)
+
+console.log("Subtraction Operators")
+// FRUITS
+let fruits1:number=10
+let fruits2:number=6
+const fruitsSubtract:number=fruits1-fruits2
+console.log("Total Fruits after subtract", fruitsSubtract)
+// VEGITABLES
+let vegi1: number = 10
+let vegi2: number = 3
+const vegiSubtract:number=vegi1-vegi2
+console.log("Total Vegitable after subtract", vegiSubtract)
+// VEHICLES
+let vechl1:number=35
+let vechl2:number=5
+const vechicle_Subtract:number=vechl1-vechl2
+console.log("Total Vicheles after subtract", vechicle_Subtract)
+//STATIONARY
+let stationary_1:number=56
+let stationary_2:number=51
+const stationary_Subtract:number=stationary_1-stationary_2
+console.log("Total Stationary after subtract", stationary_Subtract)
+// ELECTRICAL ITEMS
+let elect_1:number=10
+let elect_2:number=4
+const elect_Subtract:number=elect_1-elect_2
+console.log("Total Electrical Items after subtract", elect_Subtract)
+
+console.log("Divition Operators")
+// FRUITS
+let apples:number=30
+let no0fPerson:number=5
+const apple1Modulest:number=apples/no0fPerson
+console.log("Each person will get",apple1Modulest)
+// // VEGITABLES
+let veg1: number = 10         
+let veg2: number = 3
+const vegSubtract:number=veg1/veg2
+console.log("Total Vegitable after Divition", vegSubtract)
+// VEHICLES
+let vehcl1:number=35
+let vehcl2:number=5
+const vechicleSubtract:number=vehcl1/vehcl2
+console.log("Total Vicheles after Divition", vechicleSubtract)
+// STATIONARY
+let stationary1:number=56
+let stationary2:number=51
+const stationarySubtract:number=stationary1/stationary2
+console.log("Total Stationary after Divition", stationarySubtract)
+// ELECTRICAL ITEMS
+let elect1:number=10
+let elect2:number=4
+const electSubtract:number=elect1/elect2
+console.log("Total Electrical Items after Divition", electSubtract)
+
+console.log("Modulest Operators")
+// FRUITS
+let apples1:number=21
+let no0fPerson1:number=5
+const appleModulest:number=apples1%no0fPerson1
+console.log("Each person will get Modulest",appleModulest)
+// // VEGITABLES
+let vegitable1: number = 185        
+let vegitable2: number = 12
+const vegitableModulest:number=vegitable1%vegitable2
+console.log("Total Vegitable after Modulest", vegitableModulest)
+// VEHICLES
+let vechicle1:number=63
+let vechicle2:number=22
+const vechicleModulest:number=vechicle1%vechicle2
+console.log("Total Vicheles after Modulest", vechicleModulest)
+// STATIONARY
+let stationer1:number=21
+let stationer2:number=5
+const stationerModulest:number=stationer1%stationer2
+console.log("Total Stationary after Modulest", stationerModulest)
+// ELECTRICAL ITEMS
+let electric1:number=23
+let electric2:number=5
+const electricModulest:number=electric1%electric2
+console.log("Total Electrical Items after Modulest", electricModulest)
+
+console.log("Concatination Operators")
+// My Full Name
+let firstName:string="Saleem"
+let lastName:string="Khan"
+console.log(firstName+lastName)
+
+// My Location
+let area:string="Shah Faisal Colony"
+let city:string="Karachi"
+let country:string="Pakistan"
+console.log(area+city+country)
+
+console.log("Concatination Operators With White Space")
+// My Full Name
+let first_Name:string="Saleem"
+let last_Name:string="Khan"
+console.log(first_Name+" "+last_Name)
+
+// My Location
+let area1:string="Shah Faisal Colony"
+let city1:string="Karachi"
+let country1:string="Pakistan"
+console.log(area1+", "+city1+", "+country1)
+
+console.log("Comperison Operators")
+console.log(15==15)
+console.log(15===15)
+console.log(10 !=10)
+console.log(5 > 10)
+console.log(5 < 10)
+console.log(5 <= 10)
+console.log(2 <= 5)
+console.log(2 >= 5)
+
+
+
+
+let m=15
+m=m+5
+console.log(m)
+
+let n=25
+n+=5
+console.log(n)
